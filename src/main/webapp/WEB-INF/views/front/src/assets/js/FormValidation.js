a4c9925196bcb3c47121f0c60cdbead53e283213
prometej <nazar-danilyak@mail.ru>
2017-12-26 19:13:19 +0200

    fix

---
+++ src/main/webapp/WEB-INF/views/front/src/assets/js/FormValidation.js
@@ -0,0 +1,59 @@
+
+window.addEventListener("load",function(){
+    document.getElementById("regBtn").addEventListener("click",function(){
+        //get
+        var inp1 = document.getElementById("pas1"),
+            inp2 = document.getElementById("pas2"),
+            userName = document.getElementById("userName"),
+            lastName = document.getElementById("userLastName");
+        //value
+        var chec = document.getElementById("pas1").value,
+            checName = document.getElementById("userName").value,
+            checkLastName = document.getElementById("userLastName").value;
+
+        if(checkLastName.indexOf(' ') > 0){
+            alert("Поле з прізвищем не допускає пробіл")
+            lastName.value = "";
+            lastName.style.borderColor = "red";
+        }
+
+        if(checName.indexOf(' ') > 0){
+            alert("Поле з іменем не допускає пробіл")
+            userName.value = "";
+            userName.style.borderColor = "red";
+        }
+
+        if (chec.length < 6){
+            alert("Пароль надто короткий")
+            inp1.value = "";
+            inp2.value = "";
+            inp1.style.borderColor = "red";
+            inp2.style.borderColor = "red";
+        }
+
+        if(inp1.value!=inp2.value){
+            alert("Пароли не совпадают")
+            inp1.value = "";
+            inp2.value = "";
+        }
+    });
+})
+
+function generateSome() {
+    var html = document.getElementById("userContent");
+    html.innerHTML = "<div style='background: red;width: 100px;height: 100px'></div>";
+}
+
+//generationHTML
+function generatePhotoSelect() {
+
+    document.getElementById("userContent").innerHTML = " <div>\n" +
+        "<form:form action=\"saveImage\" method=\"post\" enctype=\"multipart/form-data\">\n" +
+        "<input type=\"file\" path=\"image\" name=\"image\">\n" +
+        "<button type=\"submit\">Do something crazy</button>\n" +
+        "</form:form>\n" +
+        "</div>"
+
+}
+
+
---
+++ src/main/webapp/WEB-INF/views/front/src/assets/js/content.js
@@ -0,0 +1,23 @@
+
+
+
+
+function generatePhotoSelect() {
+
+    document.getElementById("userContent").innerHTML = "<div class="+container>""+
+    "<div class="+row+">"+
+    "<div class="+container+">"+
+    "<div class="+row+">"+
+        "<div class="+col-md-3 +col-sm-4 +col-xs-6+"><img class="+img-responsive+"</div>"+
+        "<div class="+col-md-3 +col-sm-4 +col-xs-6+"><img class="+img-responsive+"</div>"+
+        "<div class="+col-md-3 +col-sm-4 +col-xs-6+"><img class="+img-responsive+"</div>"+
+        "<div class="+col-md-3 +col-sm-4 +col-xs-6+"><img class="+img-responsive+"</div>"+
+        "<div class="+col-md-3 +col-sm-4 +col-xs-6+"><img class="+img-responsive+"</div>"+
+        "<div class="+col-md-3 +col-sm-4 +col-xs-6+"><img class="+img-responsive+"</div>"+
+        "<div class="+col-md-3 +col-sm-4 +col-xs-6+"><img class="+img-responsive+"</div>"+
+        "</div>"+
+    "</div>"+
+    "</div>"+
+    "</div>"
+
+}
